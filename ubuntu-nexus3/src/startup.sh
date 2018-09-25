7640cf1c1f51834b2576d52ebf047955a4e62662
Fabio Jun Takada Chino <fjtc@users.noreply.github.com>
2018-09-25 01:21:28 -0300

    Nexus is no longer running as root.

--- ubuntu-nexus3/src/startup.sh
+++ ubuntu-nexus3/src/startup.sh
@@ -6,22 +6,22 @@ pushd "$MY_HOME"
 MY_HOME=$(pwd)
 popd
 
-# Check if it is the first deployment
-if ! [ -d /sonatype-work ]; then
-	echo "Creating the initial data directory..."
-	mkdir /sonatype-work
-fi
 # Populate the initial data if necessary
 if ! [ -d /sonatype-work/nexus3 ]; then
 	echo "Creating the initial deployment data from the template..."
 	cp -r $MY_HOME/sonatype-work.tpl/* /sonatype-work
 fi
+
 # Fix the link to the data directory
 if ! [ -e $MY_HOME/sonatype-work ]; then
 	echo "Create the link to the data directory volume..."
 	ln -s /sonatype-work $MY_HOME/sonatype-work
 fi
 
-cd $MY_HOME/current/bin
-./nexus run
+# Fix the user rights
+chown -R nexus:nexus /app
+chown -R nexus:nexus /sonatype-work
+
+# Run sonarqube
+su -c "$MY_HOME/current/bin/nexus run" nexus
 
